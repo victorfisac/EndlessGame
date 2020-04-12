8c098709025176025239c8f1d62744ea34902dce
victorfisac <victorfisac@gmail.com>
2020-04-12 22:42:23 +0200

    Implemented component to fix shaders in Editor

---
+++ Assets/Scripts/Helpers/AssetBundlesShaderFixer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace EndlessGame.Helpers
+{
+    public class AssetBundlesShaderFixer : MonoBehaviour
+    {
+        #if UNITY_EDITOR
+        private void Awake()
+        {
+            Graphic[] _graphics = GetComponentsInChildren<Graphic>();
+
+            for (int i = 0, count = _graphics.Length; i < count; i++)
+            {
+                Graphic _graphic = _graphics[i];
+                
+                if (_graphic.material == null)
+                    continue;
+                
+                if (_graphic.material.shader == null)
+                    continue;
+
+                _graphic.material.shader = Shader.Find(_graphic.material.shader.name);
+            }
+        }
+        #endif
+    }
+}
