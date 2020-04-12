edf507bd4792a7bd9e47a94b6f964565d6b2e946
victorfisac <victorfisac@gmail.com>
2020-04-12 23:20:14 +0200

    Added frametime component

---
+++ Assets/Scripts/Helpers/FrametimeComponent.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace EndlessGame.Helpers
+{
+    public class FrametimeComponent : MonoBehaviour
+    {
+        private string label = string.Empty;
+        private float count = 0f;
+
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        
+        private IEnumerator Start()
+        {
+            GUI.depth = 2;
+
+            while (true)
+            {
+                if (Time.timeScale > 0f)
+                {
+                    yield return new WaitForSeconds (0.1f);
+
+                    count = 1f/Time.deltaTime;
+                    label = string.Concat("FPS :", Mathf.Round (count));
+                }
+                else
+                {
+                    label = "PAUSE";
+                }
+
+                yield return new WaitForSeconds (0.5f);
+            }
+        }
+        
+        private void OnGUI ()
+        {
+            GUI.Label(new Rect(5, 40, 100, 25), label);
+        }
+    }
+}
