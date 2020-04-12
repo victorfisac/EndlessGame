13715c651bd6a66ae261cfe6a5614c9a91800022
victorfisac <victorfisac@gmail.com>
2020-04-12 18:58:01 +0200

    Refactor of project structure to support asset bundles

---
+++ Assets/Scripts/Helpers/AssetBundlesProvider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using BestHTTP;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace EndlessGame.Helpers
+{
+    public class AssetBundlesProvider
+    {
+        private static AssetBundlesProvider m_instance = null;
+        private static object m_lock = new object();
+
+        private const string BASE_URL = "https://www.victorfisac.com/files/endlessgame/";
+
+
+        public void LoadAssetBundle(string pAssetBundle, Action<AssetBundle> pOnAssetBundleLoaded = null)
+        {
+            string _uri = string.Concat(BASE_URL, pAssetBundle);
+            
+            HTTPRequest _request = new HTTPRequest(new Uri(_uri), HTTPMethods.Get, (req, res) => {
+                if (res.IsSuccess)
+                {
+                    AssetBundleCreateRequest _bundleRequest = AssetBundle.LoadFromMemoryAsync(res.Data);
+                    _bundleRequest.completed += (op) => { 
+                        if (op.isDone)
+                        {
+                            pOnAssetBundleLoaded(_bundleRequest.assetBundle);
+                        }
+                        else
+                        {
+                            Debug.LogErrorFormat("AssetBundlesProvider: failed to load asset bundle '{0}'.", pAssetBundle);
+                        }
+                    };
+                }
+                else
+                {
+                    Debug.LogErrorFormat("AssetBundlesProvider: failed to download asset bundle '{0}'.\n{1}", pAssetBundle, res.DataAsText);
+                }
+
+                req.Dispose();
+                res.Dispose();
+            });
+
+            _request.Send();
+        }
+
+
+        public static AssetBundlesProvider Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    m_instance = new AssetBundlesProvider();
+                }
+
+                return m_instance;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Modules/Loading/LoadingScreen.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using EndlessGame.Helpers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace EndlessGame.Modules.Loading
+{
+    public class LoadingScreen : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField]
+        private string[] assetBundles;
+
+        [Header("Animation")]
+        [SerializeField]
+        private float loadingDelay;
+        [SerializeField]
+        private Animator loadingAnimator;
+
+
+        private int m_loadedBundles = 0;
+
+        private const string ANIMATOR_GOTO_TRIGGER = "GoToOut";
+        private const string NEXT_SCENE_NAME = "Menu";
+
+
+        private void Awake()
+        {
+            for (int i = 0, count = assetBundles.Length; i < count; i++)
+            {
+                AssetBundlesProvider.Instance.LoadAssetBundle(assetBundles[i], OnAssetBundleLoaded);
+            }
+        }
+
+        private IEnumerator GoToMenu()
+        {
+            loadingAnimator.SetTrigger(ANIMATOR_GOTO_TRIGGER);
+
+            yield return new WaitForSeconds(loadingDelay);
+
+            SceneManager.LoadSceneAsync(NEXT_SCENE_NAME, LoadSceneMode.Additive).completed += OnSceneLoaded;
+        }
+
+        private void OnAssetBundleLoaded(AssetBundle pAssetBundle)
+        {
+            m_loadedBundles++;
+
+            if (m_loadedBundles == assetBundles.Length)
+            {
+                StartCoroutine(GoToMenu());
+            }
+        }
+
+        private void OnSceneLoaded(AsyncOperation pOperation)
+        {
+            if (pOperation.isDone)
+            {
+                SceneManager.UnloadSceneAsync(0);
+            }
+            else
+            {
+                Debug.LogErrorFormat("LoadingScreen: failed to load '{0}' scene.", NEXT_SCENE_NAME);
+            }
+        }
+    }
+}
