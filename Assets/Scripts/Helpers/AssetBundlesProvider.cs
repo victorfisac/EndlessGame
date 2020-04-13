1f2286d16a947c2226192a16fb7dcd22cb40dbc0
victorfisac <victorfisac@gmail.com>
2020-04-13 18:20:55 +0200

    Added method to retrieve asset from bundle

--- Assets/Scripts/Helpers/AssetBundlesProvider.cs
+++ Assets/Scripts/Helpers/AssetBundlesProvider.cs
@@ -11,6 +11,7 @@ namespace EndlessGame.Helpers
         private static AssetBundlesProvider m_instance = null;
         private static object m_lock = new object();
 
+
         private Dictionary<string, AssetBundle> m_bundlesDict = new Dictionary<string, AssetBundle>();
 
         private const string BASE_URL = "https://www.victorfisac.com/files/endlessgame/";
@@ -79,6 +80,23 @@ namespace EndlessGame.Helpers
             });
 
             _request.Send();
+        }
+
+        public void GetAssetFromBundle<T>(string pAssetName, string pAssetBundle, Action<T> pOnAssetRetrieved = null) where T : UnityEngine.Object
+        {
+            if (!m_bundlesDict.ContainsKey(pAssetBundle))
+            {
+                Debug.LogErrorFormat("AssetBundlesProvider: failed to retrieve asset '{0}' from asset bundle '{1}'.", pAssetName, pAssetBundle);
+                return;
+            }
+
+            AssetBundle _assetBundle = m_bundlesDict[pAssetBundle];
+            AssetBundleRequest _request = _assetBundle.LoadAssetAsync(pAssetName);
+
+            _request.completed += (op) => {
+                if (pOnAssetRetrieved != null)
+                    pOnAssetRetrieved(_request.asset as T);
+            };
         }
 
 
