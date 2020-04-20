71722cafa53c4c81505de43146a80c90c9d94fcf
victorfisac <victorfisac@gmail.com>
2020-04-20 02:04:22 +0200

    Implemented component to initialize dynamic configuration

---
+++ Assets/Scripts/Components/GooglePlayGamesComponent.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable 0649
+
+
+using EndlessGame.Services;
+using UnityEngine;
+
+
+namespace EndlessGame.Components
+{
+    public class GooglePlayGamesComponent : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField]
+        private GooglePlayGamesData config;
+
+
+        private void Start()
+        {
+            GooglePlayGamesService _service = GooglePlayGamesService.Instance;
+            _service.Initialize(config);
+        }
+    }
+}
